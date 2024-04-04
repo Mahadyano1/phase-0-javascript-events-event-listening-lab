786542bf77a9ff3e82e2f83fb59e844572cb4652
Abdimahad hassan salat <mahadgaceyte1@gmail.com>
2024-04-04 13:04:31 +0300

    initializing lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
 }
+
+function addingEventListener() {
+    let input=document.getElementById('button')
+    function showAlert() {
+        alert('I was clicked!');
+      }
+    input.addEventListener("click",showAlert)
+}
+
+addingEventListener()
